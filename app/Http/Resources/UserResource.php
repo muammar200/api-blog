f48d0c1e488f1869b53d8d9f56148c047a70b5e3
muammar200 <amuammar01@gmail.com>
2023-12-20 14:56:25 +0800

    Create UserResource

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    public $success;
+    public $message;
+    public $httpCode;
+
+    public function __construct($resource, $success, $message)
+    {
+        parent::__construct($resource);
+        $this->success = $success;
+        $this->message = $message;
+        // $this->httpCode = $httpCode;
+    }
+
+    public function toArray(Request $request): array
+    {
+        return [
+            'success' => $this->success,
+            'message' => $this->message,
+            'data' => [
+                'id' => $this->id,
+                'firstname' => $this->firstname,
+                'lastname' => $this->lastname,
+                'email' => $this->email,
+                'email_verified_at' => $this->email_verified_at,
+                'created_at' => Carbon::parse($this->created_at)->format('Y/m/d H:i:s'),
+            ],
+        ];
+    }
+
+    // public function toResponse($request)
+    // {
+    //     return parent::toResponse($request)->setStatusCode($this->httpCode);
+    // }
+}
