f7c378f53a7892106c5f756c025e5f017bd61377
muammar200 <amuammar01@gmail.com>
2024-01-04 14:28:15 +0800

    add middleware admin in Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,6 +64,8 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'admin' => \App\Http\Middleware\IsAdmin::class,
         'user-owner' => \App\Http\Middleware\UserOwner::class,
+        // 'post-owner' => \App\Http\Middleware\PostOwner::class,
     ];
 }
