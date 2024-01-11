439701ac6551dd59acad05f1eb1c0fc5f9790d6f
muammar200 <amuammar01@gmail.com>
2024-01-11 14:31:33 +0800

    create model CategoryPost

---
+++ app/Models/CategoryPost.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Post;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class CategoryPost extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['name', 'slug'];
+        
+    protected $table = 'category_posts';
+
+
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class);
+    }
+
+}
