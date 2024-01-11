16950f7c0ed5b2506a02776237cc6fc78979ebe8
muammar200 <amuammar01@gmail.com>
2024-01-11 14:33:08 +0800

    create model Like

---
+++ app/Models/Like.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Like extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'post_id'];
+
+    protected $with = ['user'];
+    public function user() : BelongsTo
+    { 
+        return $this->belongsTo(User::class, 'user_id', 'id' );
+    }
+
+    public function post() : BelongsTo
+    {
+        return $this->belongsTo(Post::class, 'post_id', 'id');
+    }
+}
