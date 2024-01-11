0b9014ccd761b3e5961540bdf70e68cde5b83dba
muammar200 <amuammar01@gmail.com>
2024-01-11 14:36:07 +0800

    create model Post

---
+++ app/Models/Post.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Like;
+use App\Models\User;
+use App\Models\Comment;
+use App\Models\PostImage;
+use App\Models\CategoryPost;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Post extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['title', 'category_id', 'slug', 'content', 'author_id', 'published_at'];
+    protected $with = ['category', 'author', 'postImages', 'comments', 'likes'];
+
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'author_id', 'id');
+    }
+
+    public function postImages(): HasMany
+    {
+        return $this->hasMany(PostImage::class, 'post_id', 'id');
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(CategoryPost::class, 'category_id', 'id');
+    }
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class, 'post_id', 'id');
+    }
+
+    public function likes(): HasMany
+    {
+        return $this->hasMany(Like::class, 'post_id', 'id');
+    }
+
+}
