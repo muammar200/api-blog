11a9049ef69d66e1135883d81636b54d19e43055
muammar200 <amuammar01@gmail.com>
2024-01-11 14:23:07 +0800

    delete default comment in RedirectIfAuthenticated

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -10,11 +10,6 @@ use Symfony\Component\HttpFoundation\Response;
 
 class RedirectIfAuthenticated
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
     public function handle(Request $request, Closure $next, string ...$guards): Response
     {
         $guards = empty($guards) ? [null] : $guards;
@@ -28,3 +23,4 @@ class RedirectIfAuthenticated
         return $next($request);
     }
 }
+
