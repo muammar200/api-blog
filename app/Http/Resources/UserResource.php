9205a617ac505d2ac5cb5957b47d769bd4a0b7ad
muammar200 <amuammar01@gmail.com>
2024-01-21 01:24:19 +0800

    modify : add token user to UserResource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -11,17 +11,19 @@ class UserResource extends JsonResource
     public $success;
     public $message;
     public $httpCode;
+    public $tokenUser;
 
-    public function __construct($resource, $success, $message)
+    public function __construct($resource, $tokenUser, $success, $message)
     {
         parent::__construct($resource);
         $this->success = $success;
         $this->message = $message;
+        $this->tokenUser = $tokenUser;
         // $this->httpCode = $httpCode;
     }
 
     public function toArray(Request $request): array
-    {
+{
         return [
             'success' => $this->success,
             'message' => $this->message,
@@ -31,8 +33,9 @@ class UserResource extends JsonResource
                 'email' => $this->email,
                 'email_verified_at' => $this->email_verified_at,
                 'created_at' => Carbon::parse($this->created_at)->format('Y/m/d H:i:s'),
-                'detail_user' => $this->whenLoaded('detailUser')
+                'detail_user' => $this->detailUser
             ],
+            'token_user' => $this->tokenUser,
         ];
     }
 
